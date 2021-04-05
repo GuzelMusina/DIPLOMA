3ae944c9280b9f0c1779c3694e14465f14a6cef1
GuzelMusina <guzmus@rambler.ru>
2021-04-05 12:49:46 +0300

    Add data mining with student csv and add it to main

--- python/main.py
+++ python/main.py
@@ -4,31 +4,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math
 
-from python.changer import ChangerStudents
-
+from python.ChangerStudents import ChangerStudents
 
 # загрузим данные
-def readCSV():
-    dataset_marks = pd.read_csv('../data/Marks.csv')
+def readCSVStudents():
+    # dataset_marks = pd.read_csv('../data/Marks.csv')
     dataset_students = pd.read_csv('../data/Students.csv')
-
-    print(dataset_students.head())
+    return dataset_students
+    # print(dataset_students.head())
 
 def refactorStudentsCSV(dataset_students):
     cs = ChangerStudents(dataset_students)
+    cs.changeBirthplace()
+    cs.changeCategoryId()
+    cs.changeInstitute()
+    cs.changeIsMedale()
+    cs.changeCreativeActive()
+    cs.changeKindtraining()
+    cs.changeOlimpiade()
+    cs.changeQualification()
+    cs.changeSchool()
     cs.changeStudyType()
+    cs.changeTypeOfTraining()
+    cs.addFIO()
+    cs.dropColumns()
+    cs.saveAs()
+    print("2nd ---------", cs.dataset_students.head())
 
-# fio_arr = []
-# for j in range(0,len(dataset_students)):
-#     st = str(dataset_students['NAME'][j]) + str(' ') + str(dataset_students['FAMILIA'][j]) + str(' ') + str(dataset_students['OTCHESTVO'][j])
-#     fio_arr.append(st)
-#
-# dataset_students.drop(['NAME', 'FAMILIA', 'OTCHESTVO','BIRTHDATE', 'RESIDENCEPLACE','VKURL',
-#                        'FACEBOOK', 'INSTAURL', 'EMAIL', 'EMAILKPFU', 'FOTO', 'PLACEOFGRADUATION',
-#                        'COUNTRY', 'REGION','DISTRICT','CITY','LOCALITY', 'ORDERBEGIN', 'ORDEREND',
-#                        'PLACEOFGRADUATION', 'YEAROFGRADUATION','REASONFORLEAVING', 'DATEADMISION',
-#                        'STUDSTATUS', 'CITIZENSHIP_COUNTRY_ID', 'CITIZENSHIP_ID'], axis='columns', inplace=True)
-# dataset_students.insert(1, 'FIO', fio_arr, True)
+
 
 # dataset_students.KINDTRAINING.unique()
 # dataset_students.TYPETRAINING.unique()
@@ -44,14 +47,14 @@ def refactorStudentsCSV(dataset_students):
 #dataset_students.STUDY_PLAN_ID.unique()
 
 
-# dataset_students.to_csv('Students_new.csv')
+
 # dataset_students["STUDENTID"] = dataset_students["STUDENTID"].apply(pd.to_numeric, errors='ignore')
 # data = pd.merge(dataset_students, dataset_marks, on='STUDENTID')
 # data.to_csv('Data.csv')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    readCSV()
+    dataset_students = readCSVStudents()
+    print("1st---------", dataset_students.head())
+    refactorStudentsCSV(dataset_students)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
