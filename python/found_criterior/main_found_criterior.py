3d6ccaaa66700fff4569a50e4655d03605b65e53
GuzelMusina <guzmus@rambler.ru>
2021-05-11 16:18:53 +0300

    add found criterior full algorithm

--- python/found_criterior/main_found_criterior.py
+++ python/found_criterior/main_found_criterior.py
@@ -4,16 +4,19 @@ from python.helpers.Reader import Reader
 from python.helpers.Saver import Saver
 
 
-from python.found_criterior.Refactor import Refactor
+# from python.found_criterior.Refactor import Refactor
 
 import python.found_criterior.methods_for_criterior.FoundCriterior as foundCriterior
+
+from python.found_criterior.methods_for_criterior.FeautureSelector import FeatureSelector
 import python.found_criterior.methods_for_criterior.T_SNE as tsne
+
 
 reader = Reader()
 saver = Saver()
 
 if __name__ == '__main__':
-    df = reader.readCSV('Data.csv')
+    # df = reader.readCSV('Data.csv')
     # df.drop(['GROUP_NUMBER'], axis='columns', inplace=True)
     # df.drop(['STUDENTID', 'FI', 'FIO'], axis='columns', inplace=True)
     # df["BALLSTOTAL"] = pd.to_numeric(df["BALLSTOTAL"], downcast='integer')
@@ -32,19 +35,19 @@ if __name__ == '__main__':
     # print(len(df1[df1['SUCCESS']=='success'])/len(df1))
     # print(len(df1[df1['SUCCESS'] == 'partly success']) / len(df1))
 
-    y_ = [0 for x in range(len(df))]
-    for i in range(len(df)):
-        if df.loc[i, 'BALLSTOTAL'] < 56:
-            y_[i] = 0
-        elif df.loc[i, 'BALLSTOTAL'] >= 56 and df.loc[i, 'BALLSTOTAL'] < 71:
-            y_[i] = 1
-        elif df.loc[i, 'BALLSTOTAL'] >= 71 and df.loc[i, 'BALLSTOTAL'] < 86:
-            y_[i] = 2
-        elif df.loc[i, 'BALLSTOTAL'] >= 86:
-            y_[i] = 3
-    df.insert(21, 'CLASS', y_, True)
-    df.to_csv('Data_with_class.csv', index=False)
-
+    # y_ = [0 for x in range(len(df))]
+    # for i in range(len(df)):
+    #     if df.loc[i, 'BALLSTOTAL'] < 56:
+    #         y_[i] = 0
+    #     elif df.loc[i, 'BALLSTOTAL'] >= 56 and df.loc[i, 'BALLSTOTAL'] < 71:
+    #         y_[i] = 1
+    #     elif df.loc[i, 'BALLSTOTAL'] >= 71 and df.loc[i, 'BALLSTOTAL'] < 86:
+    #         y_[i] = 2
+    #     elif df.loc[i, 'BALLSTOTAL'] >= 86:
+    #         y_[i] = 3
+    # df.insert(21, 'CLASS', y_, True)
+    # df.to_csv('Data_with_class.csv', index=False)
+    foundCriterior
 
 
 
--- python/found_criterior/methods_for_criterior/FoundCriterior.py
+++ python/found_criterior/methods_for_criterior/FoundCriterior.py
@@ -1,209 +1,212 @@
-W = [1, 0.1, 0.1, 0.1, 0.5,
-     0.1, 0.1, 0.1, 0.5, 0.1, 0.5
-    , 0.1, 0.5, 0.1, 0.5, 0.1, 0.5, 0.1, 0.5
-    , 0.1, 0.5, 0.5, 1, 0.5, 0.5, 0.1, 0.1, 0.1, 2]
+import pandas as pd
+import matplotlib.pyplot as plt
+# Сброс ограничений на число столбцов
+pd.set_option('display.max_columns', None)
+import math
+import statistics
+from python.found_criterior.methods_for_criterior.FeautureSelector import FeatureSelector
 
-matrix = {'COUNT_ACTIVITIES': [45, 57, 50, 50], 'SEX': [1, 1, 1, 1],
-          'BIRTHPLACE': [0, 0, 0, 0], 'INSTITUTEID': [1, 1, 1, 1],
-          'ISOBCHAGA': [0, 0, 0, 0], 'MARTIALSTATUSID': [0, 0, 0, 0],
-          'PUBLICATIONS': [0, 0, 0, 0], 'CONFERENCE': [0, 0, 0, 0],
-          'STUDACTIVE': [0, 0, 0, 0], 'CREATICEACTIVE': [0, 0, 0, 0],
-          'SOCIALACTIVE': [0, 0, 0, 0], 'MEDALE': [0, 0, 0, 0],
-          'OLIMPIADE': [0, 0, 0, 0], 'TYPEOFSCHOOL': [0, 0, 0, 0],
-          'COUNT_METTINGS': [3, 12, 15, 4], 'TIME_AUDIO_MINUTES': [280, 650, 850, 300],
-          'TIME_VIDEO_MINUTES': [50, 190, 250, 60], 'SHARE_SCREEN_MINUTES': [0, 1, 1, 0],
-          'TET_A_TET_CALLS': [0, 0, 0, 0], 'MS_MESSAGES': [0, 4, 5, 1], 'BALLSTOTAL': [56, 71, 86, 100]
-          }
-
-
-def algorithmFoundCriterio(df, PERCENTAGE):
-    x = 0
-    P = df.columns
-    print("priznaki: ", P)
-
-    classes = []
-    classes.append(df[df['CLASS'] == 0])
-    classes.append(df[df['CLASS'] == 1])
-    classes.append(df[df['CLASS'] == 2])
-    classes.append(df[df['CLASS'] == 3])
-    k = 0
-    # for i in range(10):
-    #     for p in range(len(P)):
-    #             # print(len(classes[k][classes[k][P[p]] >= 45]) / len(classes[k]))
-    #             while format(len(classes[k][classes[k][P[p]]> x ]) / len(classes[k]), '.1f') != PERCENTAGE:
-    #                 print(x, ' ', k)
-    #                 x = x + 1
-    #             else:
-    #                 print(x)
-    #                 matrix[p][k] = x
-    #                 x = 0
-    #                 if(k!=3):
-    #                     k = k+1
-    #                 else:
-    #                     k=0
-
-    print(matrix)
-
-    parametrs = 0
-    success = []
+df = pd.read_csv('Data_with_class.csv')
+df_class0 = df[df['CLASS'] == 0]
+df_class1 = df[df['CLASS'] == 1]
+df_class2 = df[df['CLASS'] == 2]
+df_class3 = df[df['CLASS'] == 3]
+# df.drop('INSTITUTEID', axis='columns', inplace=True)
+# df.to_csv('Data_with_class.csv', index=False)
+def normalizer(df):
+    arr = []
+    min_ = min(df)
+    max_ = max(df)
     for i in range(len(df)):
-        student = df.loc[i]
-        stud_sum = 0
-        if student['CLASS'] == 0:
-            k = 0
-        elif student['CLASS'] == 1:
-            k = 1
-        elif student['CLASS'] == 2:
-            k = 2
+        arr.append((df[i] - min_) / (max_ - min_))
+
+    return arr
+
+
+def replacements(df, param):
+    arr = normalizer(df[param].values)
+    df.loc[:, param] = arr
+    return df
+
+#     print(df.head(2))
+
+df_class0.drop(df_class0[df_class0['COUNT_ACTIVITIES']>1000].index, inplace=True)
+df_class1.drop(df_class1[df_class1['COUNT_ACTIVITIES']>1000].index, inplace=True)
+df_class2.drop(df_class2[df_class2['COUNT_ACTIVITIES']>1000].index, inplace=True)
+df_class3.drop(df_class3[df_class3['COUNT_ACTIVITIES']>1000].index, inplace=True)
+df_class0 = replacements(df_class0, 'COUNT_ACTIVITIES')
+df_class1 = replacements(df_class1, 'COUNT_ACTIVITIES')
+df_class2 = replacements(df_class2, 'COUNT_ACTIVITIES')
+df_class3 = replacements(df_class3, 'COUNT_ACTIVITIES')
+
+# df_class0 = replacements(df_class0, 'INSTITUTEID')
+# df_class1 = replacements(df_class1, 'INSTITUTEID')
+# df_class2 = replacements(df_class2, 'INSTITUTEID')
+# df_class3 = replacements(df_class3, 'INSTITUTEID')
+
+df_class0 = replacements(df_class0, 'PUBLICATIONS')
+df_class1 = replacements(df_class1, 'PUBLICATIONS')
+df_class2 = replacements(df_class2, 'PUBLICATIONS')
+df_class3 = replacements(df_class3, 'PUBLICATIONS')
+
+df_class0 = replacements(df_class0, 'CONFERENCE')
+df_class1 = replacements(df_class1, 'CONFERENCE')
+df_class2 = replacements(df_class2, 'CONFERENCE')
+df_class3 = replacements(df_class3, 'CONFERENCE')
+
+df_class0 = replacements(df_class0, 'MEDALE')
+df_class1 = replacements(df_class1, 'MEDALE')
+df_class2 = replacements(df_class2, 'MEDALE')
+df_class3 = replacements(df_class3, 'MEDALE')
+
+df_class0 = replacements(df_class0, 'TYPEOFSCHOOL')
+df_class1 = replacements(df_class1, 'TYPEOFSCHOOL')
+df_class2 = replacements(df_class2, 'TYPEOFSCHOOL')
+df_class3 = replacements(df_class3, 'TYPEOFSCHOOL')
+
+df_class0.drop(df_class0[df_class0['COUNT_METTINGS']>140].index, inplace=True)
+df_class1.drop(df_class1[df_class1['COUNT_METTINGS']>140].index, inplace=True)
+df_class2.drop(df_class2[df_class2['COUNT_METTINGS']>140].index, inplace=True)
+df_class3.drop(df_class3[df_class3['COUNT_METTINGS']>140].index, inplace=True)
+df_class0 = replacements(df_class0, 'COUNT_METTINGS')
+df_class1 = replacements(df_class1, 'COUNT_METTINGS')
+df_class2 = replacements(df_class2, 'COUNT_METTINGS')
+df_class3 = replacements(df_class3, 'COUNT_METTINGS')
+
+df_class0.drop(df_class0[df_class0['TIME_AUDIO_MINUTES']>10000].index, inplace=True)
+df_class1.drop(df_class1[df_class1['TIME_AUDIO_MINUTES']>10000].index, inplace=True)
+df_class2.drop(df_class2[df_class2['TIME_AUDIO_MINUTES']>10000].index, inplace=True)
+df_class3.drop(df_class3[df_class3['TIME_AUDIO_MINUTES']>10000].index, inplace=True)
+df_class0 = replacements(df_class0, 'TIME_AUDIO_MINUTES')
+df_class1 = replacements(df_class1, 'TIME_AUDIO_MINUTES')
+df_class2 = replacements(df_class2, 'TIME_AUDIO_MINUTES')
+df_class3 = replacements(df_class3, 'TIME_AUDIO_MINUTES')
+
+df_class0.drop(df_class0[df_class0['TIME_VIDEO_MINUTES']>4000].index, inplace=True)
+df_class1.drop(df_class1[df_class1['TIME_VIDEO_MINUTES']>4000].index, inplace=True)
+df_class2.drop(df_class2[df_class2['TIME_VIDEO_MINUTES']>4000].index, inplace=True)
+df_class3.drop(df_class3[df_class3['TIME_VIDEO_MINUTES']>4000].index, inplace=True)
+df_class0 = replacements(df_class0, 'TIME_VIDEO_MINUTES')
+df_class1 = replacements(df_class1, 'TIME_VIDEO_MINUTES')
+df_class2 = replacements(df_class2, 'TIME_VIDEO_MINUTES')
+df_class3 = replacements(df_class3, 'TIME_VIDEO_MINUTES')
+
+df_class0.drop(df_class0[df_class0['SHARE_SCREEN_MINUTES']>2000].index, inplace=True)
+df_class1.drop(df_class1[df_class1['SHARE_SCREEN_MINUTES']>2000].index, inplace=True)
+df_class2.drop(df_class2[df_class2['SHARE_SCREEN_MINUTES']>2000].index, inplace=True)
+df_class3.drop(df_class3[df_class3['SHARE_SCREEN_MINUTES']>2200].index, inplace=True)
+df_class0 = replacements(df_class0, 'SHARE_SCREEN_MINUTES')
+df_class1 = replacements(df_class1, 'SHARE_SCREEN_MINUTES')
+df_class2 = replacements(df_class2, 'SHARE_SCREEN_MINUTES')
+df_class3 = replacements(df_class3, 'SHARE_SCREEN_MINUTES')
+
+df_class0 = replacements(df_class0, 'TET_A_TET_CALLS')
+df_class1 = replacements(df_class1, 'TET_A_TET_CALLS')
+df_class2 = replacements(df_class2, 'TET_A_TET_CALLS')
+df_class3 = replacements(df_class3, 'TET_A_TET_CALLS')
+
+df_class0.drop(df_class0[df_class0['MS_MESSAGES']>200].index, inplace=True)
+df_class1.drop(df_class1[df_class1['MS_MESSAGES']>200].index, inplace=True)
+df_class2.drop(df_class2[df_class2['MS_MESSAGES']>200].index, inplace=True)
+df_class3.drop(df_class3[df_class3['MS_MESSAGES']>200].index, inplace=True)
+df_class0 = replacements(df_class0, 'MS_MESSAGES')
+df_class1 = replacements(df_class1, 'MS_MESSAGES')
+df_class2 = replacements(df_class2, 'MS_MESSAGES')
+df_class3 = replacements(df_class3, 'MS_MESSAGES')
+
+# проверка
+# columns = df_class3.columns[0:20]
+# for i in range(len(columns)):
+#     print(max(df_class3[columns[i]]))
+data = df.loc[:,['COUNT_ACTIVITIES', 'SEX', 'BIRTHPLACE', 'ISOBCHAGA',
+       'PUBLICATIONS', 'CONFERENCE', 'STUDACTIVE', 'CREATICEACTIVE',
+       'SOCIALACTIVE', 'TYPEOFSCHOOL', 'COUNT_METTINGS',
+       'TIME_AUDIO_MINUTES', 'TIME_VIDEO_MINUTES', 'SHARE_SCREEN_MINUTES',
+       'TET_A_TET_CALLS', 'MS_MESSAGES']]
+
+# from feature_selector import FeatureSelector
+df_labels = df.loc[:,'CLASS'].values
+# Признаки - в train, метки - в train_labels
+fs = FeatureSelector(data = data, labels = df_labels)
+# task – classification или regression в зависимости от задачи;
+# eval_metric – метрика для ранней остановки (не требуется, если ранняя остановка отключена);
+# n_iterations – количество прогонов обучения для усреднения важности;
+# early_stopping – включение/отключение ранней остановки.
+fs.identify_zero_importance(task = 'regression',
+                            eval_metric = 'auc',
+                            n_iterations = 10,
+                             early_stopping = True)
+
+zero_importance_features = fs.ops['zero_importance']
+
+X_MAJOR = 7
+
+
+def count_sum_major_and_minor_index(df):
+    major_features = ['COUNT_ACTIVITIES', 'SHARE_SCREEN_MINUTES', 'TIME_VIDEO_MINUTES',
+                      'TIME_AUDIO_MINUTES', 'COUNT_METTINGS', 'MS_MESSAGES', 'BALLSTOTAL']
+
+    sum_major = []
+    list_index = df.index
+    for i in range(len(list_index)):
+        sum_major.append((sum(df.loc[list_index[i], major_features])))
+
+    count = 0
+
+    return sum_major
+
+
+def criterior_success(sum_major):
+    return (sum_major / X_MAJOR)
+
+
+sums_class0 = count_sum_major_and_minor_index(df_class0)
+sums_class1 = count_sum_major_and_minor_index(df_class1)
+sums_class2 = count_sum_major_and_minor_index(df_class2)
+sums_class3 = count_sum_major_and_minor_index(df_class3)
+
+crit_succ_ub = statistics.mean(sums_class3)
+crit_cl_succ_ub = statistics.mean(sums_class1)
+crit_no_succ_ub = statistics.mean(sums_class0)
+
+
+def classification(sums_value, crit_succ_ub ,crit_cl_succ_ub, crit_no_succ_ub):
+    arr_succ_words = []
+    arr_succ_int = []
+    for i in range(len(sums_value)):
+        if (sums_value[i]>=crit_succ_ub):
+            arr_succ_words.append('SUCCESS')
+            arr_succ_int.append(2)
+#             print('SUCCESS')
+        elif sums_value[i] >=crit_cl_succ_ub:
+            arr_succ_words.append('CLOSE TO SUCCESS')
+            arr_succ_int.append(1)
+# #             print('CLOSE TO SUCCESS')
+        elif sums_value[i] >=crit_no_succ_ub:
+            arr_succ_words.append('NO SUCCESS')
+            arr_succ_int.append(0)
+#             print('NO TO SUCCESS')
         else:
-            k = 3
+            arr_succ_words.append('NO SUCCESS')
+            arr_succ_int.append(0)
+    return arr_succ_words, arr_succ_int
 
-        if student['COUNT_ACTIVITIES'] >= matrix['COUNT_ACTIVITIES'][k]:
-            # print('TRUE COUNT_ACTIVITIES')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-        if student['SEX'] == matrix['SEX'][k]:
-            # print('TRUE SEX')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-        if student['BIRTHPLACE'] == matrix['BIRTHPLACE'][k]:
-            # print('TRUE BIRTHPLACE')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
+cl0_words, cl0_int = classification(sums_class0,crit_succ_ub, crit_cl_succ_ub, crit_no_succ_ub)
+cl1_words, cl1_int = classification(sums_class1,crit_succ_ub, crit_cl_succ_ub, crit_no_succ_ub)
+cl2_words, cl2_int = classification(sums_class2,crit_succ_ub, crit_cl_succ_ub, crit_no_succ_ub)
+cl3_words, cl3_int = classification(sums_class3,crit_succ_ub, crit_cl_succ_ub, crit_no_succ_ub)
 
-        if student['INSTITUTEID'] == 4:
-            # print('VERY TRUE INSTITUTEID')
-            stud_sum = stud_sum + 1 * 0.2
-            parametrs = parametrs + 1
-        elif student['INSTITUTEID'] == matrix['INSTITUTEID'][k]:
-            # print('TRUE INSTITUTEID')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
 
-        if student['ISOBCHAGA'] == matrix['ISOBCHAGA'][k]:
-            # print('TRUE ISOBCHAGA')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
+df_class0.insert(21, "SUCCESS_METRICS_WORDS", cl0_words, True)
+df_class0.insert(21,"SUCCESS_METRICS_NUMBERS", cl0_int, True)
 
-        if student['MARTIALSTATUSID'] == matrix['MARTIALSTATUSID'][k]:
-            # print('TRUE MARTIALSTATUSID')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
+df_class1.insert(21, "SUCCESS_METRICS_WORDS", cl1_words, True)
+df_class1.insert(21,"SUCCESS_METRICS_NUMBERS", cl1_int, True)
+df_class2.insert(21, "SUCCESS_METRICS_WORDS", cl2_words, True)
+df_class2.insert(21,"SUCCESS_METRICS_NUMBERS", cl2_int, True)
+df_class3.insert(21, "SUCCESS_METRICS_WORDS", cl3_words, True)
+df_class3.insert(21,"SUCCESS_METRICS_NUMBERS", cl3_int, True)
 
-        if student['PUBLICATIONS'] > 0:
-            # print('VERY TRUE PUBLICATIONS')
-            stud_sum = stud_sum + 1 * 0.5
-            parametrs = parametrs + 1
-        elif student['PUBLICATIONS'] == matrix['PUBLICATIONS'][k]:
-            # print('TRUE PUBLICATIONS')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        if student['CONFERENCE'] > 0:
-            # print('VERY TRUE CONFERENCE')
-            stud_sum = stud_sum + 1 * 0.5
-            parametrs = parametrs + 1
-        elif student['CONFERENCE'] == matrix['CONFERENCE'][k]:
-            # print('TRUE CONFERENCE')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        if student['STUDACTIVE'] == 1:
-            # print('VERY TRUE STUDACTIVE')
-            stud_sum = stud_sum + 1 * 0.2
-            parametrs = parametrs + 1
-        elif student['STUDACTIVE'] == matrix['STUDACTIVE'][k]:
-            # print('TRUE STUDACTIVE')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        if student['CREATICEACTIVE'] == 1:
-            # print('VERY TRUE CREATICEACTIVE')
-            stud_sum = stud_sum + 1 * 0.2
-            parametrs = parametrs + 1
-        elif student['CREATICEACTIVE'] == matrix['CREATICEACTIVE'][k]:
-            # print('TRUE CREATICEACTIVE')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        if student['SOCIALACTIVE'] == 1:
-            # print('VERY TRUE SOCIALACTIVE')
-            stud_sum = stud_sum + 1 * 0.2
-            parametrs = parametrs + 1
-        elif student['SOCIALACTIVE'] == matrix['SOCIALACTIVE'][k]:
-            # print('TRUE SOCIALACTIVE')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        if student['MEDALE'] > 0:
-            # print('VERY TRUE MEDALE')
-            stud_sum = stud_sum + 1 * 0.5
-            parametrs = parametrs + 1
-        elif student['MEDALE'] == matrix['MEDALE'][k]:
-            # print('TRUE MEDALE')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        if student['OLIMPIADE'] == 1:
-            # print('VERY TRUE OLIMPIADE')
-            stud_sum = stud_sum + 1 * 0.5
-            parametrs = parametrs + 1
-        elif student['OLIMPIADE'] == matrix['OLIMPIADE'][k]:
-            # print('TRUE OLIMPIADE')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        if student['TYPEOFSCHOOL'] == 1:
-            # print('VERY TRUE TYPEOFSCHOOL')
-            stud_sum = stud_sum + 1 * 0.5
-            parametrs = parametrs + 1
-        elif student['TYPEOFSCHOOL'] == matrix['TYPEOFSCHOOL'][k]:
-            # print('TRUE TYPEOFSCHOOL')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        if student['COUNT_METTINGS'] >= matrix['COUNT_METTINGS'][k]:
-            # print('TRUE COUNT_METTINGS')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        if student['TIME_AUDIO_MINUTES'] >= matrix['TIME_AUDIO_MINUTES'][k]:
-            # print('TRUE TIME_AUDIO_MINUTES')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        if student['TIME_VIDEO_MINUTES'] >= matrix['TIME_VIDEO_MINUTES'][k]:
-            # print('TRUE TIME_VIDEO_MINUTES')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-        if student['SHARE_SCREEN_MINUTES'] >= matrix['SHARE_SCREEN_MINUTES'][k]:
-            # print('TRUE SHARE_SCREEN_MINUTES')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-        if student['TET_A_TET_CALLS'] >= matrix['TET_A_TET_CALLS'][k]:
-            # print('TRUE TET_A_TET_CALLS')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-        if student['MS_MESSAGES'] >= matrix['MS_MESSAGES'][k]:
-            # print('TRUE MS_MESSAGES')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-        if student['BALLSTOTAL'] <= matrix['BALLSTOTAL'][k]:
-            # print('TRUE BALLSTOTAL')
-            stud_sum = stud_sum + 1 * W[parametrs]
-            parametrs = parametrs + 1
-
-        parametrs = 0
-        # print(stud_sum)
-
-        if stud_sum >= 6:
-            success.append('success')
-        elif stud_sum>=5:
-            success.append('partly success')
-        else:
-            success.append('-')
-
-    df.insert(1, 'SUCCESS', success, True)
-    df.to_csv('Success_data.csv')
-    # print(success)
-
+new_df = pd.concat([df_class0, df_class1, df_class2, df_class3])
+new_df.sort_index(inplace=True)
+new_df.to_csv('New_data.csv', index=False)
