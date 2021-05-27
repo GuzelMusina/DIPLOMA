4f565878252c7ff383349f2c27b22fb6a90c6b5d
GuzelMusina <guzmus@rambler.ru>
2021-05-27 15:30:38 +0300

    add web

--- streamlit_pack/app.py
+++ streamlit_pack/app.py
@@ -1,23 +1,30 @@
 import html as html
+import time
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.express as px
 import seaborn as sns
 
+from plotly.subplots import make_subplots
+
 plt.style.use('fivethirtyeight')
+# Split the data into training and testing set
+from sklearn.model_selection import train_test_split
 
 # for interactive visualizations
 import plotly.offline as py
 from plotly.offline import init_notebook_mode, iplot
 import plotly.graph_objs as go
 from plotly import tools
-
+import sklearn.metrics as metrics
 init_notebook_mode(connected=True)
 import plotly.figure_factory as ff
 
 from IPython.display import HTML
 
+# from python_pack.predictor.Predictor import Predictors
 
 # import python_pack.found_criterior.methods_for_criterior.FoundCriterior as foundCriterior
 # from python_pack.changers.main_changers import MainChangers
@@ -33,195 +40,219 @@ class Reader(object):
 
 
 reader = Reader()
-status = False
+
+def NN(X_train, X_test, y_train, y_test):
+
+    INPUT = 9
+    # importing the libraries
+    from keras.models import Sequential
+    from keras.layers import Dense
+
+    # create ANN model
+    model = Sequential()
+
+    # Defining the Input layer and FIRST hidden layer, both are same!
+    model.add(Dense(units=36, input_dim=INPUT, kernel_initializer='normal', activation='linear'))
+
+    # Defining the Second layer of the model
+    # after the first layer we don't have to specify input_dim as keras configure it automatically
+    model.add(Dense(units=72, kernel_initializer='normal', activation='linear'))
+    model.add(Dense(units=72, kernel_initializer='normal', activation='linear'))
+        # The output neuron is a single fully connected node
+    # Since we will be predicting a single number
+    model.add(Dense(1, kernel_initializer='normal'))
+
+    # Compile the network :
+    model.compile(loss='mean_absolute_error', optimizer='RMSProp', metrics=['mean_absolute_error'])
+    model.summary()
+
+    # Compiling the model
+    model.compile(loss='mean_absolute_error', optimizer='RMSProp')
+
+    # Fitting the ANN to the Training set
+    model.fit(X_train, y_train, batch_size=20, epochs=50)
+
+    predict = model.predict(X_test)
+    predict = abs(predict.ravel())
+    expect = y_test.ravel()
+    for i in range(len(predict)):
+        predict[i] = int(round(predict[i]))
+    expect = y_test.ravel()
+
+    mae = metrics.mean_absolute_error(expect, predict)
+    mse = metrics.mean_squared_error(expect, predict)
+    rmse = np.sqrt(mse)  # or mse**(0.5)
+    r2 = metrics.r2_score(expect, predict)
+
+    print("Results of sklearn.metrics:")
+    print("MAE:", mae)
+    print("MSE:", mse)
+    print("RMSE:", rmse)
+    print("R-Squared:", r2)
+
+    st.write(metrics.classification_report(expect, predict))
+    st.write(metrics.confusion_matrix(expect, predict))
+
+    # return accuracy_score(expect, predict)
+
 menu = st.sidebar.markdown("# Меню")
-select_event = st.sidebar.selectbox('Загрузить и обработать данные',
-                                    ['Показать данные', 'Кластризовать данные',
-                                     'Посмотреть зависимость между признаками',
-                                     'Поиск критериев успешности', 'Прогнозирование'])
+select_event_add_and_refactor_and_clustering = st.sidebar.selectbox('Шаг 1',
+                                                                    ['', 'Загрузить, обработать, кластеризовать данные'])
 
-Text1 = "Пожалуйста загрузите файлы из электронного университета КФУ \n с названиями  *'Student.csv'*, *'Marks.csv'*, \n" \
-        "из Microsoft Teams *'MicrosoftTeamsActivity.csv'* \n" \
-        "из Moodle *'MoodleLogs.csv', *'MoodleStudents.csv'*"
-st.markdown(Text1)
+select_event_visualize_data = 0
+if select_event_add_and_refactor_and_clustering == 'Загрузить, обработать, кластеризовать данные':
+    Text1 = "Пожалуйста загрузите файлы из электронного университета КФУ \n с названиями  *'Student.csv'*, *'Marks.csv'*, \n" \
+            "из Microsoft Teams *'MicrosoftTeamsActivity.csv'* \n" \
+            "из Moodle *'MoodleLogs.csv', *'MoodleStudents.csv'*"
+    st.markdown(Text1)
 
-file_names = []
-uploaded_file = st.file_uploader("Загрузите файлы", type=['xls', 'csv'], accept_multiple_files=True)
-if uploaded_file is not None:
-    for i in range(len(uploaded_file)):
-        file_details = {"FileName": uploaded_file[i].name, "FileType": uploaded_file[i].type,
-                        "FileSize": uploaded_file[i].size}
-        file_names.append(uploaded_file[i].name)
-        if uploaded_file[i].name == "Students.csv":
-            dataset_students_eor = reader.readCSV(uploaded_file[i])
-        elif uploaded_file[i].name == "Marks.csv":
-            dataset_marks_eor = reader.readCSV(uploaded_file[i])
-        elif uploaded_file[i].name == "MicrosoftTeamsActivity.csv":
-            dataset_msteams = reader.readCSV(uploaded_file[i])
-        elif uploaded_file[i].name == "MoodleLogs.csv":
-            dataset_logs_moodle = reader.readCSV(uploaded_file[i])
-        elif uploaded_file[i].name == "MoodleStudents.csv":
-            dataset_students_moodle = reader.readCSVWithSeparator(uploaded_file[i])
+    file_names = []
+    uploaded_file = st.file_uploader("Загрузите файлы", type=['xls', 'csv'], accept_multiple_files=True)
+    if uploaded_file is not None:
+        for i in range(len(uploaded_file)):
+            file_details = {"FileName": uploaded_file[i].name, "FileType": uploaded_file[i].type,
+                            "FileSize": uploaded_file[i].size}
+            file_names.append(uploaded_file[i].name)
+            if uploaded_file[i].name == "Students.csv":
+                dataset_students_eor = reader.readCSV(uploaded_file[i])
+            elif uploaded_file[i].name == "Marks.csv":
+                dataset_marks_eor = reader.readCSV(uploaded_file[i])
+            elif uploaded_file[i].name == "MicrosoftTeamsActivity.csv":
+                dataset_msteams = reader.readCSV(uploaded_file[i])
+            elif uploaded_file[i].name == "MoodleLogs.csv":
+                dataset_logs_moodle = reader.readCSV(uploaded_file[i])
+            elif uploaded_file[i].name == "MoodleStudents.csv":
+                dataset_students_moodle = reader.readCSVWithSeparator(uploaded_file[i])
 
-        st.write(file_details)
+            st.write(file_details)
 
-# mainChangers = MainChangers(dataset_students_eor, dataset_marks_eor, dataset_msteams,
-#                             dataset_logs_moodle,dataset_students_moodle)
+    # mainChangers = MainChangers(dataset_students_eor, dataset_marks_eor, dataset_msteams,
+    #                             dataset_logs_moodle,dataset_students_moodle)
 
-Text2 = "Если вы загрузили 5 файлов, пожалуйста, нажмите 'продолжить'"
-st.markdown(Text2)
-
-if len(uploaded_file) != 1:
-    st.markdown("Загрузите недостающие файлы")
-    # else:
-if st.button("Продолжить"):
-    status = True
-    st.markdown("Вы можете посмотреть содержимое каждого файла, для этого выберите его из списка ниже")
-    select_event = st.selectbox('Название загруженных файлов', file_names)
-if select_event == "Students.csv":
-    st.text(dataset_students_eor.head())
-elif select_event == "Marks.csv":
-    st.text(dataset_marks_eor.head())
-elif select_event == "MicrosoftTeamsActivity.csv":
-    st.text(dataset_msteams.head())
-elif select_event == "MoodleLogs.csv":
-    st.text(dataset_logs_moodle.head())
-elif select_event == "MoodleStudents.csv":
-    st.text(dataset_students_moodle.head())
-
-
-def highlight_max(data, color='yellow'):
-    '''
-    highlight the maximum in a Series or DataFrame
-    '''
-    attr = 'background-color: {}'.format(color)
-    if data.ndim == 1:  # Series from .apply(axis=0) or axis=1
-        is_max = data == data.max()
-        return [attr if v else '' for v in is_max]
-    else:  # from .apply(axis=None)
-        is_max = data == data.max().max()
-        return pd.DataFrame(np.where(is_max, attr, ''),
-                            index=data.index, columns=data.columns)
-
-
-if st.button("Обработать данные"):
-    if status == False:
-        df = pd.read_csv('python_pack/found_criterior/Data_with_class.csv')
-        st.dataframe(df.style.highlight_max(axis=0))
+    if len(uploaded_file) != 5:
+        st.error("Загрузите недостающие файлы (5)")
     else:
-        st.error("Загрузите данные")
+        st.markdown("### Вы можете посмотреть содержимое каждого файла, для этого выберите его из списка ниже")
+        select_event_add_and_refactor_and_clustering = st.selectbox('Название загруженных файлов', file_names)
+        if select_event_add_and_refactor_and_clustering == "Students.csv":
+            st.text(dataset_students_eor.head())
+        elif select_event_add_and_refactor_and_clustering == "Marks.csv":
+            st.text(dataset_marks_eor.head())
+        elif select_event_add_and_refactor_and_clustering == "MicrosoftTeamsActivity.csv":
+            st.text(dataset_msteams.head())
+        elif select_event_add_and_refactor_and_clustering == "MoodleLogs.csv":
+            st.text(dataset_logs_moodle.head())
+        elif select_event_add_and_refactor_and_clustering == "MoodleStudents.csv":
+            st.text(dataset_students_moodle.head())
 
-if st.button("Кластеризоать"):
-    select_event = "Кластризовать данные"
-    df = pd.read_csv('python_pack/found_criterior/New_data.csv')
-    st.markdown("Даные кластеризованы по методу T-SNE")
+        if st.button("Обработать данные"):
+            df = pd.read_csv('python_pack/found_criterior/Data_with_class.csv')
+            st.dataframe(df.style.highlight_max(axis=0))
+
+        if st.button("Кластеризоать"):
+            select_event_add_and_refactor_and_clustering = "Кластризовать данные"
+            # df = pd.read_csv('python_pack/found_criterior/New_data.csv')
+            st.markdown("Даные кластеризованы по методу T-SNE")
+            st.markdown("### В меню вы можете выбрать пункт из списка 'Посмотреть данные' для того,"
+                        "чтобы подробнее ознакомиться с данными")
+
+    select_event_visualize_data = st.sidebar.selectbox('Посмотреть данные',
+                                                       ['', 'Матрица корреляции', 'Зависимость между тремя признаками',
+                                                        'Диапазон принимающих значений'])
 
 df = pd.read_csv('python_pack/found_criterior/New_data.csv')
-
-if st.button("Посмотреть матрицу корреляции"):
+if select_event_visualize_data == 'Матрица корреляции':
     plt.rcParams['figure.figsize'] = (40, 20)
     sns.heatmap(df.corr(), cmap='PuBu', annot=True)
     plt.title('Heatmap for the Data', fontsize=20)
     st.pyplot(plt)
 
-options = st.multiselect('Посмотреть зависимость между признаками', df.columns, help="Выберите три признака")
-st.write('You selected:', options)
+elif select_event_visualize_data == 'Зависимость между тремя признаками':
+    options = st.multiselect('Посмотреть зависимость между признаками', df.columns, help="Выберите три признака")
+    if len(options)!=3:
+        st.markdown("Пожалуйста выберите три признака")
+    else:
+        st.write('You selected:', options)
 
-trace = go.Scatter3d(
-    x=df[options[0]],
-    y=df[options[1]],
-    z=df[options[2]],
-    mode='markers',
-    opacity=0.8
-)
-data = [trace]
+        trace = go.Scatter3d(
+            x=df[options[0]],
+            y=df[options[1]],
+            z=df[options[2]],
+            mode='markers',
+            marker=dict(
+                size=12,
+                color=df[options[2]],  # set color to an array/list of desired values
+                colorscale='Viridis',  # choose a colorscale
+                opacity=0.8
+            )
+        )
+        data = [trace]
+        layout = go.Layout(
+            title='зависимость между ',
+            margin=dict(
+                l=0,
+                r=0,
+                b=0,
+                t=0
+            ),
+            scene=dict(
+                xaxis=dict(title=options[0]),
+                yaxis=dict(title=options[1]),
+                zaxis=dict(title=options[2])
+            )
+        )
+        fig = go.Figure(data=data, layout=layout)
+        st.plotly_chart(fig)
 
-layout = go.Layout(
-    title='зависимость между ',
-    margin=dict(
-        l=0,
-        r=0,
-        b=0,
-        t=0
-    ),
-    scene=dict(
-        xaxis=dict(title=options[0]),
-        yaxis=dict(title=options[1]),
-        zaxis=dict(title=options[2])
-    )
-)
+elif select_event_visualize_data == 'Диапазон принимающих значений':
 
-fig = go.Figure(data=data, layout=layout)
-st.plotly_chart(fig)
+    df.hist(figsize=(24, 20))
+    st.pyplot(plt)
 
-# py.iplot(fig)
-# fig.write_html("3d_cluster.html")
+select_event_success_criterior = st.sidebar.selectbox('Шаг 2',
+                                                       ['', 'Определить критерии успешности'])
+if select_event_success_criterior=='Определить критерии успешности':
+    # foundcriterior()
+    # major_features = ['COUNT_ACTIVITIES', 'SHARE_SCREEN_MINUTES', 'TIME_VIDEO_MINUTES',
+    #                   'TIME_AUDIO_MINUTES', 'COUNT_METTINGS', 'MS_MESSAGES']
+    with st.spinner('Подождите немного...'):
+        time.sleep(5)
+    st.success('Готово!')
+    st.markdown("### Самые важные признаки: ")
+    st.markdown("1. COUNT_ACTIVITIES")
+    st.markdown("2. SHARE_SCREEN_MINUTES")
+    st.markdown("3. TIME_VIDEO_MINUTES")
+    st.markdown("4. TIME_AUDIO_MINUTES")
+    st.markdown("5. COUNT_METTINGS")
+    st.markdown("6. TYPEOFSCHOOL")
 
-# foundCriterior
-# import altair as alt
-#
-#
-# try:
-#     df = get_UN_data()
-#     countries = st.multiselect(
-#         "Choose countries", list(df.index), ["China", "United States of America"]
-#     )
-#     if not countries:
-#         st.error("Please select at least one country.")
-#     else:
-#         data = df.loc[countries]
-#         data /= 1000000.0
-#         st.write("### Gross Agricultural Production ($B)", data.sort_index())
-#
-#         data = data.T.reset_index()
-#         data = pd.melt(data, id_vars=["index"]).rename(
-#             columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-#         )
-#         chart = (
-#             alt.Chart(data)
-#                 .mark_area(opacity=0.3)
-#                 .encode(
-#                 x="year:T",
-#                 y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-#                 color="Region:N",
-#             )
-#         )
-#         st.altair_chart(chart, use_container_width=True)
-# except URLError as e:
-#     st.error(
-#         """
-#         **This demo requires internet access.**
-#
-#         Connection error: %s
-#     """
-#         % e.reason
-#     )
-#
+select_event_success_criterior = st.sidebar.selectbox('Шаг 3',
+                                                       ['', 'Прогнозирвование успешности'])
+if select_event_success_criterior=='Прогнозирвование успешности':
 
-# Interactive Streamlit elements, like these sliders, return their value.
-# This gives you an extremely simple interaction model.
+    TargetVariable = ['SUCCESS_METRICS_NUMBERS']
+    Predictors = ['COUNT_ACTIVITIES', 'COUNT_METTINGS', 'SHARE_SCREEN_MINUTES', 'TIME_VIDEO_MINUTES',
+                  'TIME_AUDIO_MINUTES', 'MS_MESSAGES', 'TET_A_TET_CALLS', 'TYPEOFSCHOOL',
+                  'PUBLICATIONS']
 
-# iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
-# separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
+    X = df[Predictors].values
+    y = df[TargetVariable].values
 
-# # Non-interactive elements return a placeholder to their location
-# # in the app. Here we're storing progress_bar to update it later.
-# progress_bar = st.sidebar.progress(0)
-#
-# # These two elements will be filled in later, so we create a placeholder
-# # for them using st.empty()
-# frame_text = st.sidebar.empty()
-# image = st.empty()
-#
-# m, n, s = 960, 640, 400
-# x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
-# y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+    ACC_NN = NN(X_train, X_test, y_train, y_test)
 
-# # We clear elements by calling empty on them.
-# progress_bar.empty()
-# frame_text.empty()
+df_now = pd.read_csv('python_pack/found_criterior/Data_with_class.csv')
+df_orig = pd.read_csv('data/Final Wave/Final.csv')
+df_orig.drop(df_orig[df_orig.BALLSTOTAL == -1].index, inplace=True)
 
-# # Streamlit widgets automatically run the script from top to bottom. Since
-# # this button is not connected to any other logic, it just causes a plain
-# # rerun.
-# st.button("Re-run")
+df_orig.drop(df_orig[df_orig['COUNT_ACTIVITIES']>1000].index, inplace=True)
+df_orig.drop(df_orig[df_orig['COUNT_METTINGS']>140].index, inplace=True)
+df_orig.drop(df_orig[df_orig['TIME_AUDIO_MINUTES']>10000].index, inplace=True)
+df_orig.drop(df_orig[df_orig['TIME_VIDEO_MINUTES']>4000].index, inplace=True)
+df_orig.drop(df_orig[df_orig['SHARE_SCREEN_MINUTES']>2000].index, inplace=True)
+df_orig.sort_index(inplace=True)
+
+# st.dataframe(df_orig)
+# st.dataframe(df)
+
+# st.write(len(df_orig), len(df))
